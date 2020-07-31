febec7d88f7e861fdddeaaa7146524ec3297c64a
WLeondike <billyleondike@gmail.com>
2020-07-31 15:31:07 -0400

    added Workout model

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+//modules
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+//PORT
 const PORT = process.env.PORT || 3000;
 
+//set app to Express
 const app = express();
 
+//url processing + JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//static folder
 app.use(express.static("public"));
 
+//mongoDB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 require("./routes/htmlRoutes")(app);
